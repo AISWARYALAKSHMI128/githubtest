c016025f9f895a533f0cbca5070444192e217d62
AISWARYALAKSHMI128 <156852992+AISWARYALAKSHMI128@users.noreply.github.com>
2024-01-21 21:17:56 -0500

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,21 @@
+# Taking user input for the character and the string
+chr= input("Enter the character to check: ")
+if chr=='v' or chr=='V':
+    print("present")
+elif chr=='i' or chr=='I':
+    print('present')
+elif chr=='b' or chr=='B':
+    print('present')
+elif chr=='g' or chr=='G':
+    print('present')
+elif chr=='y' or chr=='Y':
+    print('present')
+else:
+    print('invalid')
+  
+
+# Checking if the character is present in the string
+# if character_to_check in input_string:
+#     print("The character '{character_to_check}' is present in the string.")
+# else:
+#     print(f"The character '{character_to_check}' is not present in the string.")
